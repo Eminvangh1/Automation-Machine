4f3e5403da93cfefe0c040c6db69dcce2fc4c4c9
eminy <eminy@EMONN>
2024-09-26 16:33:09 +0300

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace ConsoleApp13
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+         
+            string[] products = { "Water", "Wafer", "CocoCola", "biscuit" };
+
+
+            double[] prices = { 10, 15, 17.50, 12.50 };                     
+
+              for (int i = 0; i < products.Length; i++)
+
+            {
+
+                Console.WriteLine(products[i]);
+                Console.WriteLine(prices[i]);
+
+            }
+            Console.WriteLine("Select a Product");
+            int vote = Convert.ToInt32(Console.ReadLine());
+            Console.Clear();
+            double productprice = prices[vote];
+            double totalbalance = 0;
+
+            while (totalbalance < productprice)
+            {
+                Console.Write($"Products Prices: {productprice} \n Please Please Input Money");
+                double moneyinflow= Convert.ToDouble(Console.ReadLine());
+                totalbalance += moneyinflow;
+                Console.Clear();
+                if (totalbalance < productprice)
+                {
+                    Console.WriteLine($"Insufficient funds! Total: {totalbalance}  Required money: {productprice} TL");
+                }
+            }
+
+
+            Console.WriteLine($"{prices[vote]}product purchased Total Balance: {totalbalance} TL");
+
+            if (totalbalance > productprice)
+            {
+                Console.Clear();
+                Console.WriteLine($" Remaining Balance Is Refunded: {totalbalance - productprice} TL");
+
+            }
+        }
+    }
+}
